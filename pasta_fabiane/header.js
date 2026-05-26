a2f4c1a2d75444aa4de489ce516eca3d122c9680
FabianeMdeOliveira <fabianemdeoliveira8@gmail.com>
2026-05-26 14:56:07 -0300

    commit 2605

---
+++ pasta_fabiane/header.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const headerHTML = `
+        <header>
+            <a href="index.html"><img src="./CRUD-removebg-preview.png"  width="150" height="100"><></a>
+        </header>
+    `;
+    document.body.insertAdjacentHTML("afterbegin", headerHTML);
+});
